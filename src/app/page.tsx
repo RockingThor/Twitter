9d98bc3b31993d14202882437437c33e7ea5b7f5
Rohit Nandi <rohitnandibmp@gmail.com>
2024-05-29 23:47:48 +0530

    some basic components added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import Header from "@/components/header";
 import Image from "next/image";
 
 export default function Home() {
-  return <div className="">Hello World</div>;
+    return (
+        <>
+            <Header label="Home" />
+        </>
+    );
 }
--- src/components/layouts/rootLayout.tsx
+++ src/components/layouts/rootLayout.tsx
@@ -1,23 +1,25 @@
 import React from "react";
 import Sidebar from "../sidebar";
+import FollowBar from "../followbar";
 
 interface LayoutProps {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
 const RootLayoutOut = ({ children }: LayoutProps) => {
-  return (
-    <div className="h-screen bg-black">
-      <div className="container h-full mx-auto xl:px-30 max-w-6xl">
-        <div className="grid grid-cols-4 h-full">
-          <Sidebar />
-          <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
-            {children}
-          </div>
+    return (
+        <div className="h-screen bg-black">
+            <div className="container h-full mx-auto xl:px-30 max-w-6xl">
+                <div className="grid grid-cols-4 h-full">
+                    <Sidebar />
+                    <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
+                        {children}
+                    </div>
+                    <FollowBar />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default RootLayoutOut;
